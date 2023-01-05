65ab1f28780d77784d776f48b35feaddbc5322d6
Houssem Lachtar <lachtarhoussem4@gmail.com>
2023-01-05 17:35:13 +0100

    Update python_method.py

--- developer/python_method.py
+++ developer/python_method.py
@@ -4,18 +4,18 @@ class cal:
         self.x = x
         self.y = y
 
-    #在cal_add函数前加上@property，使得该函数可直接调用，封装起来   
+    #在cal_add 
     @property          
     def cal_add(self):
         return self.x + self.y
 
-    #在cal_info函数前加上@classmethon，则该函数变为类方法，该函数只能访问到类的数据属性，不能获取实例的数据属性
+    #在cal_info
     @classmethod       
-    def cal_info(cls):  #python自动传入位置参数cls就是类本身
-        print(cls.cal_name)   # cls.cal_name调用类自己的数据属性
+    def cal_info(cls):  #python
+        print(cls.cal_name)   # cls.cal_name
 
-    @staticmethod       #静态方法 类或实例均可调用
-    def cal_test(a,b,c): #改静态方法函数里不传入self 或 cls
+    @staticmethod       
+    def cal_test(a,b,c): #self cls
         print(a,b,c)
 
 
